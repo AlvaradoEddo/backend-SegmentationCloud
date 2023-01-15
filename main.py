a72ea7ad8012cac6aa85b4888275a53dba43f4d4
AlvaradoEddo <eaalvara@espol.edu.ec>
2023-01-15 17:05:22 -0500

    Version 1

---
+++ main.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from pydantic import BaseModel
+from starlette.responses import HTMLResponse
+from typing import List
+
+app = FastAPI()
+
+
+file = None;
+
+@app.get("/")
+async def main():
+    content = """
+<body>
+<form action="/uploadfile/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</form>
+</body>
+    """
+    return HTMLResponse(content=content)
+
+@app.post("/files/")
+async def create_upload_file(file: bytes = File(...)):
+    return {"file_size": len(file)}
+
+@app.post("/uploadfile/")
+async def create_upload_file(files: UploadFile):
+    if files and files.content_type == "image/tiff":
+        file=files
+        process_image(files.file)
+        return {"filename": files.filename}
+
+    raise HTTPException(status_code=400, detail="File is not a tiff image")
+
+def process_image(image: bytes = File(...)):
+    print(image.read())
+    print("prueba")
+    #return {"image_size": len(image)}
+
+@app.post("/execute")
+async def execute():
+    return {"status": "ok"}
+
+
+@app.get("/finalImage")
+async def get_final_image():
+    return {"image": "image"}
+
+@app.get("/result/")
+async def read_result():
+    return {"result": "result"}
